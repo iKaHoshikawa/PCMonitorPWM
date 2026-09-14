d6c81ab214c8fc37492938bae1e9a93718c6ee7d
Hoshikawa iKa <dengxiangyu1210@outlook.com>
2026-09-14 09:00:55 +0800

    完善1602部分代码

--- controller/sketch_001/sketch_001.ino
+++ controller/sketch_001/sketch_001.ino
@@ -2,36 +2,73 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <DFRobotDFPlayerMini.h>
 #include <SoftwareSerial.h>
-#include <LedControl.h>
 #include <SPI.h>
+#include <LiquidCrystal_I2C.h>
 
 #define LED_R 8
 #define LED_Y 7
 #define LED_G 4
+#define LCD_B 13
+#define LCD_G 14
+#define LCD_R 15
 
 DFRobotDFPlayerMini DFPlayer;
 Adafruit_PWMServoDriver pca = Adafruit_PWMServoDriver(0x40);
-LedControl m7 = LedControl(11, 13, 9, 4);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 unsigned long lastResponseTime = 0;
 bool handshaking = true;
 bool connectedornot = false;
 int playing = 0;
+int HH = 0, MM = 0;
 
 int T = 0, CL = 0, CT = 0, CP = 0, GL = 0, GT = 0, GP = 0, RL = 0, CTH = 0, GTH = 0;
 String dataBuffer = "";
 String cmdBuffer = "";
 int CLHContinue = 0, GLHContinue = 0;
 
+byte degc[8] = {
+  0b01000,
+  0b10100,
+  0b01000,
+  0b00110,
+  0b01001,
+  0b01000,
+  0b01001,
+  0b00110
+};
+
+byte cpu[8] = {
+  0b00000,
+  0b01010,
+  0b11111,
+  0b10001,
+  0b10001,
+  0b11111,
+  0b01010,
+  0b00000
+};
+
+byte gpu[8] = {
+  0b00001,
+  0b11111,
+  0b01010,
+  0b11010,
+  0b11010,
+  0b11010,
+  0b11010,
+  0b01110
+};
+
 void test() {
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < 13; i++) {
     pca.setPWM(i, 0, 4095);
   }
   digitalWrite(LED_G, HIGH);
   digitalWrite(LED_Y, HIGH);
   digitalWrite(LED_R, HIGH);
   delay(500);
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < 13; i++) {
     pca.setPWM(i, 0, 0);
   }
   digitalWrite(LED_G, LOW);
@@ -50,9 +87,14 @@ int getValue(String data, int index) {
 }
 
 void setup() {
+  delay(100);
   Serial1.begin(9600);
   Serial.begin(9600);
-  SPI.begin();
+  lcd.init();
+  lcd.createChar(0, degc);
+  lcd.createChar(1, cpu);
+  lcd.createChar(2, gpu);
+  lcd.clear();
   pinMode(LED_G, OUTPUT);
   pinMode(LED_Y, OUTPUT);
   pinMode(LED_R, OUTPUT);
@@ -61,6 +103,9 @@ void setup() {
   digitalWrite(LED_R, LOW);
   pca.begin();
   pca.setPWMFreq(1000);
+  pca.setPWM(LCD_R, 0, 1024);
+  pca.setPWM(LCD_G, 0, 1024);
+  pca.setPWM(LCD_B, 0, 1024);
   test();
   delay(500);
   if (DFPlayer.begin(Serial1)) {
@@ -73,6 +118,10 @@ void setup() {
     delay(1000);
     digitalWrite(LED_Y, LOW);
   }
+  lcd.setCursor(0, 0);
+  lcd.print("PCMonitor by iKa");
+  lcd.setCursor(0, 1);
+  lcd.print("Starting up...");
 }
 
 void loop() {
@@ -131,7 +180,6 @@ void loop() {
     dataBuffer = "";
     digitalWrite(LED_G, LOW);
   }
-
   if (handshaking) {
     digitalWrite(LED_G, LOW);
     digitalWrite(LED_Y, LOW);
@@ -145,6 +193,14 @@ void loop() {
     pca.setPWM(8, 0, 0);
     pca.setPWM(10, 0, 0);
     pca.setPWM(12, 0, 0);
+    pca.setPWM(LCD_R, 0, 1024);
+    pca.setPWM(LCD_G, 0, 1024);
+    pca.setPWM(LCD_B, 0, 1024);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Waiting for");
+    lcd.setCursor(0, 1);
+    lcd.print("input...");
   } else {
     if (CL > 460) {
       CLHContinue += 1;
@@ -168,6 +224,35 @@ void loop() {
         digitalWrite(LED_R, LOW);
         digitalWrite(LED_Y, LOW);
         digitalWrite(LED_G, HIGH);
+        pca.setPWM(LCD_R, 0, 0);
+        pca.setPWM(LCD_G, 0, 1024);
+        pca.setPWM(LCD_B, 0, 0);
+        lcd.clear();
+        HH = T / 100;
+        MM = T % 100;
+        if(HH < 10){
+          lcd.setCursor(0, 0);
+          lcd.print("0");
+          lcd.print(HH);
+          lcd.print(":");
+          lcd.print(MM);
+        }
+        else{
+          lcd.setCursor(0, 0);
+          lcd.print(HH);
+          lcd.print(":");
+          lcd.print(MM);
+        }
+        lcd.print(" ");
+        lcd.write(1);
+        lcd.print(map(CT, 0, 511, 20, 85));
+        lcd.write(0);
+        lcd.print(" ");
+        lcd.write(2);
+        lcd.print(map(GT, 0, 511, 20, 85));
+        lcd.write(0);
+        lcd.setCursor(0, 1);
+        lcd.print("(^_^)v Good!");
         DFPlayer.stop();
         playing = 0;
       } else {
@@ -181,6 +266,35 @@ void loop() {
         digitalWrite(LED_Y, HIGH);
         digitalWrite(LED_R, LOW);
         digitalWrite(LED_G, LOW);
+        pca.setPWM(LCD_R, 0, 1024);
+        pca.setPWM(LCD_G, 0, 1024);
+        pca.setPWM(LCD_B, 0, 0);
+        lcd.clear();
+        HH = T / 100;
+        MM = T % 100;
+        if(HH < 10){
+          lcd.setCursor(0, 0);
+          lcd.print("0");
+          lcd.print(HH);
+          lcd.print(":");
+          lcd.print(MM);
+        }
+        else{
+          lcd.setCursor(0, 0);
+          lcd.print(HH);
+          lcd.print(":");
+          lcd.print(MM);
+        }
+        lcd.print(" ");
+        lcd.write(1);
+        lcd.print(map(CT, 0, 511, 20, 85));
+        lcd.write(0);
+        lcd.print(" ");
+        lcd.write(2);
+        lcd.print(map(GT, 0, 511, 20, 85));
+        lcd.write(0);
+        lcd.setCursor(0, 1);
+        lcd.print("(x_x) Struggling");
         DFPlayer.stop();
         playing = 0;
       }
@@ -195,20 +309,47 @@ void loop() {
       digitalWrite(LED_Y, LOW);
       digitalWrite(LED_G, LOW);
       digitalWrite(LED_R, HIGH);
+      pca.setPWM(LCD_R, 0, 1024);
+      pca.setPWM(LCD_G, 0, 0);
+      pca.setPWM(LCD_B, 0, 0);
       if (CTH == 0 && GTH != 0 && playing != 2) {
+        lcd.clear();
         DFPlayer.stop();
         DFPlayer.loop(2);
         playing = 2;
+        lcd.setCursor(2,0);
+        lcd.print("!!High Temp!!");
+        lcd.setCursor(0,1);
+        lcd.print("GPU ");
+        lcd.print(GTH);
+        lcd.write(0);
       }
       if (CTH != 0 && GTH == 0 && playing != 3) {
+        lcd.clear();
         DFPlayer.stop();
         DFPlayer.loop(3);
         playing = 3;
+        lcd.setCursor(2,0);
+        lcd.print("!!High Temp!!");
+        lcd.setCursor(0,1);
+        lcd.print("CPU ");
+        lcd.print(CTH);
+        lcd.write(0);
       }
       if (CTH != 0 && GTH != 0 && playing != 1) {
+        lcd.clear();
         DFPlayer.stop();
         DFPlayer.loop(1);
         playing = 1;
+        lcd.setCursor(0,0);
+        lcd.print("!!!High Temp!!!");
+        lcd.setCursor(0,1);
+        lcd.print("CPU ");
+        lcd.print(CTH);
+        lcd.write(0);
+        lcd.print(" GPU ");
+        lcd.print(GTH);
+        lcd.write(0);
       }
     }
   }
